f6e3b39df6eb8d4fd38dc6466a5d7b91f09e3039
Heitor Ribeiro <heitor_481@hotmail.com>
2020-01-11 14:11:05 -0300

    Creating the project

---
+++ KneatSoftware/KneatSoftware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KneatSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
